02a0887250f519496edda80d5a649c9fbf92f710
Dorian <45897585+CodingPerson@users.noreply.github.com>
2024-10-15 18:52:16 +0800

    Add files via upload

---
+++ class_embed/run_type_name.sh
@@ -0,0 +1,6 @@
+python static_representations.py --dataset_name BBN
+python utterance_word_representations.py --dataset_name BBN
+python class_words_and_representations.py --dataset_name BBN
+python static_representations.py --dataset_name fnerd
+python utterance_word_representations.py --dataset_name fnerd
+python class_words_and_representations.py --dataset_name fnerd
